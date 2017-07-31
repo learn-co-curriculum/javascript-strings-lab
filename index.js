62e05e7f9f2cd2b34edb5b835d9c24c82ed9c6be
Gabe Jackson <gj@mail.co.de>
2017-07-30 23:47:34 -0400

    Replace old Mocha tests with new strategy and clean up files

--- index.js
+++ index.js
@@ -1,9 +1,1 @@
-var greeting = "";
-
-var specialGuest = "Neil deGrasse Tyson"
-
-var greetSpecialGuest = "" + specialGuest + "!";
-
-var topic = "space";
-
-var conversation = `${topic}`;
+// Write your code in this file!
